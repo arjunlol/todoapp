5ad177f79048711083b0b5eb2ae1ae8b98fe7fef
vheimpel <vheimpel@gmail.com>
2017-05-24 22:40:05 +0000

    Creates users and categories tables in database

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,10 +1,15 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', function (table) {
-    table.increments();
-    table.string('name');
+  return knex.schema.createTable('users', function (t) {
+    t.increments('id').unsigned().primary();
+        t.dateTime('createdAt').notNull();
+        t.dateTime('updatedAt').nullable();
+        t.string('user_name').notNull();
+        t.string('email').notNull();
+        t.string('password').notNull();
   });
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema.dropTable('users');
 };
+
