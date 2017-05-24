bfd193c442409a92f5587cfee3a24c0393d1e11c
Arjun Lall <lallarjun@gmail.com>
2017-05-24 21:47:29 +0000

    inital working with wolfram api

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,12 @@
 
 const express = require('express');
 const router  = express.Router();
+var request = require('request');
 
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
+
     knex
       .select("*")
       .from("users")
@@ -14,5 +16,18 @@ module.exports = (knex) => {
     });
   });
 
+  router.get("/wolfsearch/:search", (req, res) => {
+    console.log('test');
+    request({
+
+
+      uri: 'http://api.wolframalpha.com/v2/query',
+      qs: {
+        input: req.params.search,
+        appid: '8A2RH8-QPYYEQGL7K',
+      }
+    }).pipe(res);
+  });
   return router;
 }
+
