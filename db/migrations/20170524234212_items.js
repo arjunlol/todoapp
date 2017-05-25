7ea6dfefac83d8d7b2c9d10ddbd9fb5b01527cf1
Arjun Lall <lallarjun@gmail.com>
2017-05-25 14:28:31 +0000

    fixed database

--- db/migrations/20170524234212_items.js
+++ db/migrations/20170524234212_items.js
@@ -6,8 +6,10 @@ exports.up = function(knex, Promise) {
         t.string('name').notNull();
         t.float('latitude').nullable();
         t.float('longitude').nullable();
-        t.foreign('categories_id').references('categories.id').notNull();
-        t.foreign('users_id').references('users.id').notNull();
+        t.integer('categories_id').unsigned()
+        t.foreign('categories_id').references('categories.id');
+        t.integer('users_id').unsigned()
+        t.foreign('users_id').references('users.id');
   });
 };
 
