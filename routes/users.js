05ecb8a111714eedfbb916b7206ee916466388df
Shawna <shawnalehman@gmail.com>
2017-05-27 14:43:32 +0000

    fixed spacing

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router  = express.Router();
 const bcrypt = require('bcrypt');
 const yelpSearch  = require('./yelp.js');
-//const productCheck = require('./product_check.js')
+const productCheck = require('./product_check.js')
 
 module.exports = (knex) => {
 
