9a9c775e0cf6b9071f2050a40de20c7150eabb45
Arjun Lall <lallarjun@gmail.com>
2017-06-07 00:17:55 +0000

    change pool

--- knexfile.js
+++ knexfile.js
@@ -25,11 +25,10 @@ module.exports = {
     client: "postgresql",
     connection: process.env.DATABASE_URL + "?ssl=true",
     pool: {
-      min: 2,
-      max: 10
+      min: 0,
+      max: 100
     },
     migrations: {
-      directory: "./db/migrations",
       tableName: "migrations"
     }
   }
